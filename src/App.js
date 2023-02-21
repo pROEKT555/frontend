b8cc55114ef540a71a588a5bca296f908dd9b1fe
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-02-21 10:30:36 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Home from "./componets/Home/Hoem";
-import Registerform from "./componets/Login/Registerform";
+import { useState } from "react";
+import Registerform from "./componets/Register/Registerform";
+import Loginform from "./componets/Login/Loginform";
 import Menunav from "./componets/Menu-nav/Menunav";
 import { Route,Routes} from "react-router-dom";
 function App() {
+  const[isLoggedIn,setisLoggedIn] = useState(false);
   return (
      <div>
+      
       <Menunav/>
       <Routes>
-        <Route exact path='/' element={<Home></Home>}/>
-        <Route exact path='/login' element={<Registerform></Registerform>}/>
+        <Route exact path='/' element={<Home ></Home>}/>
+        <Route exact path='/login' element={<Loginform setisLoggedIn={setisLoggedIn}></Loginform>}/>
+        <Route exact path='/register' element={<Registerform ></Registerform>}/>
+
       </Routes>
     </div>
   );
---
+++ src/componets/Login/Loginform.jsx
@@ -0,0 +1,59 @@
+import React,{ useState } from "react";
+import "./Loginform.css"
+// import axios from "axios";
+import { Link } from "react-router-dom";
+const Loginform = (props) =>{
+  const[inputlogin,setInputlogin] = useState('');
+  const[inputPassword,setInputPassword] = useState('')
+  // const postsend =(login,passworld)=>{
+  //   axios.post('http://localhost:8000/login/', {
+  //     login: login,
+  //     passworld: passworld
+  //   })
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+  // }
+  const LoginChangeHendler = (event) =>{
+    setInputlogin(event.target.value)
+
+  }
+  const PasswordChangeHendler = (event) =>{
+    setInputPassword(event.target.value)
+
+  }
+
+  const submitHandler = (event) =>{
+    event.preventDefault();
+    props.setisLoggedIn(true);
+    // props.history.push('/');
+  }
+  return(
+
+    <div className="form-registere">
+    
+      <form className="form-registere__form" onSubmit={submitHandler}>
+        <h1 className="form-registere-title">Увійти в обліковий запис</h1>
+        <div className="registere-form__control registere-form__control-1">
+          {/* <label>Ведіть логін</label> */}
+          <input placeholder="Ведіть логін або пошту" className="registere-form__input" value={inputlogin} onChange={LoginChangeHendler} type="text" ></input>
+        </div>
+        <div className="registere-form__control">
+          {/* <label>Ведіть пароль</label> */}
+          <input placeholder="Ведіть пароль" className="registere-form__input" value={inputPassword} onChange={PasswordChangeHendler} type="password"></input> 
+        </div>        
+        <div className="registere-form__actions">
+          <button className="registere-form__actions__button" type="submit">Увійти</button>
+        </div>
+        <div className="a-register-form">
+          <Link to="/register" className="a-register-form__a">Створити обліковий запис</Link>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Loginform;
