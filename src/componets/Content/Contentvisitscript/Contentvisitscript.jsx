3fff1567a35c9af24e67d0b71bfd141e064bd66e
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-03 14:57:13 +0200

    x

--- src/componets/Content/Contentvisitscript/Contentvisitscript.jsx
+++ src/componets/Content/Contentvisitscript/Contentvisitscript.jsx
@@ -1,18 +1,18 @@
-
+import './Contentvisitscript.css'
 const Contentvisitscript = (props) =>{
   return(
     <div className="content-block">
         <div className="content-block-nameiuser">
-          Імя створювача{props.name}
+          Імя створювача:{props.name}
         </div>
         <div className="content-block-nameiuser">
-          Назва контенту{props.title}
+          Назва контенту:{props.title}
         </div>
         <div className="content-block-nameiuser">
-          Опис контенту{props.description}
+          Опис контенту:{props.description}
         </div>
         <div className="content-block-nameiuser">
-          Прикріплений файл{props.file}
+          Прикріплений файл:{props.file}
         </div>
     </div>
   )
