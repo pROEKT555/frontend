028054bef41f8707a73899f50fbeb3fd21bc0bcc
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-03 14:47:23 +0200

    x

--- src/componets/Content/Contentvisit/Contentvisit.jsx
+++ src/componets/Content/Contentvisit/Contentvisit.jsx
@@ -1,6 +1,74 @@
 import './Contentvisit.css'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Contentvisitscript from '../Contentvisitscript/Contentvisitscript';
+import { useState } from 'react';
+//   let item= [
+//     {
+//         "author_id": 1,
+//         "author_name": "zxc",
+//         "name": "zxc?",
+//         "descript": "Чому не варто звязуватися з zxc?",
+//         "files": "Томушо zxc це СФ!"
+//     },
+//     {
+//         "author_id": 1,
+//         "author_name": "zxc",
+//         "name": "zxc?",
+//         "descript": "Чому не варто звязуватися з zxc?",
+//         "files": "Томушо zxc це СФ!"
+//     },
+//     {
+//         "author_id": 1,
+//         "author_name": "zxc",
+//         "name": "zxc?",
+//         "descript": "Чому не варто звязуватися з zxc?",
+//         "files": "Томушо zxc це СФ!"
+//     },
+//     {
+//         "author_id": 1,
+//         "author_name": "zxc",
+//         "name": "a",
+//         "descript": "a",
+//         "files": "a"
+//     },
+//     {
+//         "author_id": 1,
+//         "author_name": "zxc",
+//         "name": "a",
+//         "descript": "a",
+//         "files": "a"
+//     },
+//     {
+//         "author_id": 2,
+//         "author_name": "guller",
+//         "name": "a",
+//         "descript": "a",
+//         "files": "a"
+//     },
+//     {
+//         "author_id": 2,
+//         "author_name": "guller",
+//         "name": "a",
+//         "descript": "a",
+//         "files": "a"
+//     }
+// ];
+
 const Contentvisit =() =>{
+  const[inputitem,setInputitem] = useState([]);
+  const fetchData =  () => {
+    axios.get('http://localhost:8000/content/')
+  .then(response => {
+    // console.log(response.data);
+    setInputitem(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  };
+  console.log(inputitem)
+  fetchData()
   return(
     <div className='Contentvisit'>
       <div className='conteiner'>
@@ -8,7 +76,9 @@ const Contentvisit =() =>{
           <Link to='/contentadd' className='content-add__Link'>Дотати контент</Link>
         </div>
         <div className='content-visit'>
-
+        {inputitem.map((item, index) => (
+          <Contentvisitscript key={index} name={item.author_name} title={item.name} description={item.descript} file={item.files} />
+        ))}
         </div>
       </div>
 
--- src/componets/Content/Contentvisitscript/Contentvisitscript.jsx
+++ src/componets/Content/Contentvisitscript/Contentvisitscript.jsx
@@ -1,5 +1,21 @@
-const Contentvisitscript = () =>{
-  
+
+const Contentvisitscript = (props) =>{
+  return(
+    <div className="content-block">
+        <div className="content-block-nameiuser">
+          Імя створювача{props.name}
+        </div>
+        <div className="content-block-nameiuser">
+          Назва контенту{props.title}
+        </div>
+        <div className="content-block-nameiuser">
+          Опис контенту{props.description}
+        </div>
+        <div className="content-block-nameiuser">
+          Прикріплений файл{props.file}
+        </div>
+    </div>
+  )
 }
 
 export default Contentvisitscript;
