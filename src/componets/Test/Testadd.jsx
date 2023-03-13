e8ce9dabcab89880320a97de05e7c4720008953e
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-13 21:33:02 +0200

    assa

--- src/componets/Test/Testadd.jsx
+++ src/componets/Test/Testadd.jsx
@@ -1,12 +1,44 @@
+/* eslint-disable eqeqeq */
 import { useState } from "react";
 import img from "./304947.svg"
 import axios from "axios";
 import "./Testadd.css"
+
 const Testadd = (props) =>{
   const[isNametest,setisNametest] = useState("");
   const[isIdtest,setisIdtest] = useState("");
+  const[isinputradio,setisinputradio] = useState("");
+  const[isinputquest,setisinputquest] = useState("");
+  const[isinputquest1,setisinputquest1] = useState("");
+  const[isinputquest2,setisinputquest2] = useState("");
+  const[isinputquest3,setisinputquest3] = useState("");
+  const[isinputquest4,setisinputquest4] = useState("");
+  const[isrowquest,setrowquest]= useState([]);
   const[isquest,setisquest] = useState(false);
-  let rowquest =[]
+  const ContentinputquestChange =(event)=>{
+    console.log(event.target.value)
+    setisinputquest(event.target.value)
+  }
+  const ContentradioChange =(event)=>{
+    setisinputradio(event.target.value)
+    console.log(event.target.value)
+  }
+  const Contentinputquest1Change =(event)=>{
+    setisinputquest1(event.target.value)
+    console.log(event.target.value)
+  }
+  const Contentinputquest2Change =(event)=>{
+    setisinputquest2(event.target.value)
+    console.log(event.target.value)
+  }
+  const Contentinputquest3Change =(event)=>{
+    setisinputquest3(event.target.value)
+    console.log(event.target.value)
+  }
+  const Contentinputquest4Change =(event)=>{
+    setisinputquest4(event.target.value)
+    console.log(event.target.value)
+  }
   const Sendcontent =(authid,nametest)=>{
       axios.post('http://127.0.0.1:8000/test/', {
         author:authid,
@@ -31,31 +63,151 @@ const Testadd = (props) =>{
     Sendcontent(props.isIdacaynt,isNametest)
     setisNametest('')
   }
+  const submitHandler1 = (event) =>{
+    event.preventDefault();
+    // eslint-disable-next-line eqeqeq
+    if(isinputradio==1){
+      console.log()
+      setrowquest([...isrowquest,
+      
+        {
+          "text": isinputquest,
+          "answ": [
+              {
+                "text": isinputquest1,
+                "is_true": true
+              },
+              {
+                "text": isinputquest2,
+                "is_true": false
+              },
+              {
+                "text": isinputquest3,
+                "is_true": false
+              },
+              {
+                "text": isinputquest4,
+                "is_true": false
+              }
+              
+          ]
+       }
+      ]);
+    }else if(isinputradio==2){
+      setrowquest([...isrowquest,
+      
+        {
+          "text": isinputquest,
+          "answ": [
+              {
+                "text": isinputquest1,
+                "is_true": false
+              },
+              {
+                "text": isinputquest2,
+                "is_true": true
+              },
+              {
+                "text": isinputquest3,
+                "is_true": false
+              },
+              {
+                "text": isinputquest4,
+                "is_true": false
+              }
+              
+          ]
+        }
+      ]);
+    }else if(isinputradio==3){
+      console.log(123123)
+      setrowquest([...isrowquest,
+      
+        {
+          "text": isinputquest,
+          "answ": [
+              {
+                "text": isinputquest1,
+                "is_true": false
+              },
+              {
+                "text": isinputquest2,
+                "is_true": false
+              },
+              {
+                "text": isinputquest3,
+                "is_true": true
+              },
+              {
+                "text": isinputquest4,
+                "is_true": false
+              }
+              
+          ]
+       }
+      ])
+    }else if(isinputradio==4){
+      setrowquest([...isrowquest,
+      
+        {
+          "text": isinputquest,
+          "answ": [
+              {
+                "text": isinputquest1,
+                "is_true": false
+              },
+              {
+                "text": isinputquest2,
+                "is_true": false
+              },
+              {
+                "text": isinputquest3,
+                "is_true": false
+              },
+              {
+                "text": isinputquest4,
+                "is_true": true
+              }
+              
+          ]
+       }
+      ])
+    }
+    console.log(isrowquest)
+  }
+
+    
   if(isquest===true){
     return(
       <div className="question">
        <h1>Додати запитання</h1>
         <div className="row">
-        <div className="row-block">
-            <input type='text'></input>
-          </div>
-          <div className="row-block">
-            <input className="test-radio" type='radio'name="test" />
-            <input placeholder="Питання 1" type='text'></input>
-          </div>
-          <div className="row-block">
-            <input className="test-radio" type='radio'name="test" />
-            <input placeholder="Питання 2" type='text'></input>
-          </div>
-          <div className="row-block">
-            <input className="test-radio" type='radio'name="test" />
-            <input placeholder="Питання 3" type='text'></input>
-          </div>
-          <div className="row-block">
-            <input className="test-radio" type='radio'name="test" />
-            <input placeholder="Питання 4" type='text'></input>
-          </div>
-        </div>
+        <form onSubmit={submitHandler1}>
+              <div className="row-block">
+                <input type='text' onChange={ContentinputquestChange} placeholder="Питання"></input>
+              </div>
+              <div className="row-block">
+                <input className="test-radio" value="1" onChange={ContentradioChange} type='radio'name="test" />
+                <input placeholder="Відповідь 1" onChange={Contentinputquest1Change} type='text'></input>
+              </div>
+              <div className="row-block">
+                <input className="test-radio" value="2"  onChange={ContentradioChange} type='radio'name="test" />
+                <input placeholder="Відповідь 2" onChange={Contentinputquest2Change} type='text'></input>
+              </div>
+              <div className="row-block">
+                <input className="test-radio" value="3"  onChange={ContentradioChange} type='radio'name="test" />
+                <input placeholder="Відповідь 3" onChange={Contentinputquest3Change} type='text'></input>
+              </div>
+              <div className="row-block">
+                <input className="test-radio" value="4"  onChange={ContentradioChange} type='radio'name="test" />
+                <input placeholder="Відповідь 4" onChange={Contentinputquest4Change} type='text'></input>
+              </div>
+              <div className="row-block">
+                <button type="submit">Додати</button>
+              </div>
+            </form>
+            </div>
+        
       </div>
      
     )
