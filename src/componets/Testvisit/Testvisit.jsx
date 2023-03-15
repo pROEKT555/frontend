1d70439a2a6d0cc656be77a4a788d4bc42bb31c6
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-15 15:02:44 +0200

    d

--- src/componets/Testvisit/Testvisit.jsx
+++ src/componets/Testvisit/Testvisit.jsx
@@ -1,3 +1,19 @@
-const Testvisit = () =>{
-  return
+const Testvisit = (props) =>{
+  return(
+    <div className="content-block">
+    <h2>Назва тесту{props.title}</h2>
+    <div className="content-block-nameiuser">
+      Імя створювача:{props.test1}
+    </div>
+    <div className="content-block-nameiuser">
+      Назва контенту:{props.test2}
+    </div>
+    <div className="content-block-nameiuser">
+      Опис контенту:{props.test3}
+    </div>
+    <div className="content-block-nameiuser">
+      Прикріплений файл:{props.test4}
+    </div>
+</div>
+  )
 }
