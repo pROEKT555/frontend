0b188f2cfe4f353c7cf7516b2ffe9b57d6d4e1e5
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-28 15:37:59 +0300

    s

--- src/componets/Testvisit/Testvisit.jsx
+++ src/componets/Testvisit/Testvisit.jsx
@@ -1,4 +1,9 @@
+import "./Testvisit.css"
 const Testvisit =( ) =>{
-
+  return(
+    <div className="Testvisit">
+      <div className="conteiner"></div>
+    </div>
+  )
 }
 export default Testvisit;
