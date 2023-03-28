01bf6a5bbc2d915eced11f500d83085a54ebe093
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-28 19:53:27 +0300

    XS

--- src/componets/Testvisit/Testvisit.jsx
+++ src/componets/Testvisit/Testvisit.jsx
@@ -1,14 +1,31 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Testvisit.css"
+import { useState } from 'react';
+import Testvisitscript from "../Testvisitscript/Testvisitscript";
 const Testvisit =( ) =>{
+  const[inputitem,setInputitem] = useState([]);
+  const fetchData =  () => {
+    axios.get('http://localhost:8000/test/')
+    .then(response => {
+      console.log(response.data.test_output);
+      setInputitem(response.data.test_output);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  };
+  fetchData()
   return(
     <div className="Testvisit">
       <div className="conteiner">
         <div className="button-add">
-          <Link to="/test/add" className="buttonadd">button-add</Link>
+          <Link to="/test/add"  className="buttonadd">Додати тест</Link>
         </div>
         <div className="Testvisit-row">
-          
+          {inputitem.map((item, index) => (
+            <Testvisitscript key={index} title={item.name} subtitle={item.author_name} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/componets/Testvisitscript/Testvisitscript.jsx
@@ -0,0 +1,14 @@
+const Testvisitscript = (props) =>{
+  return(
+    <div className="test-block">
+        <div className="test-block-title">
+          {props.title}
+        </div>
+        <div className="test-block-subtitle">
+          {props.subtitle}
+        </div>
+    </div>
+  )
+}
+
+export default Testvisitscript;
